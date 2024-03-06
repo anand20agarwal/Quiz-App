e052e93f18d12fdc7115e6f062bfcb2e8a872ac9
Anand Agarwal <anand20agarwal00@gmail.com>
2024-03-06 22:25:05 +0530

    Question Update

--- src/Data/QuizData.js
+++ src/Data/QuizData.js
@@ -2,32 +2,32 @@ export const QuizData = [
     {
         question: "Which is the best language",
         options:[ "Javascript" , "c" , "JAVA" , "Python" ],
-        answer: 4
+        answer: 1
     },
 
     {
         question: "Statement to print in JavsScript",
         options:[ "Console.log" , "print" , "Scanf" , "Cout" ],
-        answer: 4
+        answer: 1
     },
     {
-        question: "Which is the best language",
-        options:[ "Javascript" , "c" , "JAVA" , "Python" ],
-        answer: 4
+        question: " what result will you get? 3 > 2 > 1 === false;",
+        options:[ "True" , "False"  ],
+        answer: 2
     },
     {
         question: "HTML Stands For",
         options:[ "Hyper Text Markup Language" , "Hyper Type Markup Language" , "Hello Text Maekup Language" , "Hyper Text Markup Loader" ],
-        answer: 4
+        answer: 1
     },
     {
         question: "Which of the following keywords is used to define a variable in Javascript?",
         options:[ "var" , "let" , "Both A and B" , "None of the above" ],
-        answer: 4
+        answer: 1
     },
     {
         question: "How can a datatype be declared to be a constant type?",
-        options:[ "const" , "var" , "let" , "constant" ],
-        answer: 4
+        options:[ "let" , "var" , "const" , "constant" ],
+        answer: 3
     }
 ];
