bf97ab165f6303cf48135616141ed85ebb23193d
Anand Agarwal <anand20agarwal00@gmail.com>
2024-03-06 22:12:14 +0530

    Question Update

--- src/Data/QuizData.js
+++ src/Data/QuizData.js
@@ -16,18 +16,18 @@ export const QuizData = [
         answer: 4
     },
     {
-        question: "Which is the best language",
-        options:[ "Javascript" , "c" , "JAVA" , "Python" ],
+        question: "HTML Stands For",
+        options:[ "Hyper Text Markup Language" , "Hyper Type Markup Language" , "Hello Text Maekup Language" , "Hyper Text Markup Loader" ],
         answer: 4
     },
     {
-        question: "Which is the best language",
-        options:[ "Javascript" , "c" , "JAVA" , "Python" ],
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        options:[ "var" , "let" , "Both A and B" , "None of the above" ],
         answer: 4
     },
     {
-        question: "Which is the best language",
-        options:[ "Javascript" , "c" , "JAVA" , "Python" ],
+        question: "How can a datatype be declared to be a constant type?",
+        options:[ "const" , "var" , "let" , "constant" ],
         answer: 4
     }
 ];
